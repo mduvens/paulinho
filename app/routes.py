7903667e6db17bd13f026ac60006387fbfb62513
manuel <mduvens@gmail.com>
2020-12-27 01:14:34 +0000

    first plot

--- app/routes.py
+++ app/routes.py
@@ -1,20 +1,30 @@
 from app import app
-from flask import render_template, url_for
+from flask import render_template, url_for,Response
 from app.models import test
+import io 
+import random
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
 
 @app.route('/')
 @app.route('/index')
 def index():
     x = test.multiply()
     return render_template("inicio.html", value=x)
+    
+@app.route('/plot.png')
+def plot_png():
+    fig = create_figure()
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
 
-@app.route('/about')
-def about():
-    return render_template("pauloveado.html")
-
-@app.route('/appA')
-def appA():
-    return render_template("appA.html")
-
+def create_figure():
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+    xs = range(100)
+    ys = [random.randint(1, 50) for x in xs]
+    axis.plot(xs, ys)
+    return fig
 if __name__ == '__main__':
     app.run()
