1346d3c2cbd0703e762571477ba59f3984665f0d
manuel <mduvens@gmail.com>
2020-12-31 17:08:06 +0000

    heroku test

--- app/models/automatopy.py
+++ app/models/automatopy.py
@@ -1,21 +1,21 @@
-alfabeto = {"0","1"}
-estados = {"A","B","C","D"}
-estadoInicial = "A"
-estadosAceitacao = {"B"}
-tabelaTrans = { 
-    ("A","0"):"B", # (atualEstado,simbolo): novoEstado
-    ("A","1"):"D",
-    ("B","0"):"B",
-    ("B","1"):"C",
-    ("C","0"):"B",
-    ("C","1"):"C",
-    ("D","0"):"D",
-    ("D","1"):"D"
-}
-estado = estadoInicial
+
 
 def checkAutomato(sequencia):
-    global estado
+    alfabeto = {"0","1"}
+    estados = {"A","B","C","D"}
+    estadoInicial = "A"
+    estadosAceitacao = {"B"}
+    tabelaTrans = { 
+        ("A","0"):"B", # (atualEstado,simbolo): novoEstado
+        ("A","1"):"D",
+        ("B","0"):"B",
+        ("B","1"):"C",
+        ("C","0"):"B",
+        ("C","1"):"C",
+        ("D","0"):"D",
+        ("D","1"):"D"
+    }
+    estado = estadoInicial
     for e in sequencia:
         if e in alfabeto:
             estado = tabelaTrans[estado,e]
