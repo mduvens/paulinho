a5ad101f61d06ecb1592f715776ac0957b1e6768
manuel <mduvens@gmail.com>
2021-01-01 18:02:23 +0000

    scrap test

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,6 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-from apscheduler.scheduler import Scheduler 
-
- 
-sched = Scheduler() # Scheduler object 
-sched.start() 
- 
-def fetch_data_from_api(): 
-    ''' 
-     Do your job here 
-    ''' 
- 
-#add your job here 
-sched.add_interval_job(fetch_data_from_api,minutes=10) 
- 
-''' 
-All of your app.routes here 
-''' 
- 
-# finally 
-app.run() 
 from app import routes
 
